fee2628594af5dc1e5922270ef6099529c55a3a0
Brittany <owens.brittany@gmail.com>
2021-07-01 16:59:13 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let lodash = require("lodash");
+
+let name = "brittany is cool";
+
+console.log(lodash.upperFirst(name));
+console.log(lodash.lowerCase(name));
+console.log(lodash.upperCase(name));
+console.log(lodash.startCase(name));
+
+// use these functions from lodash:
+// lowerCase();
+// upperCase();
+// startCase();
+
+// const fixNamess = (caps) => {
+//   result = caps.toLowerCase();
+//   result = lodash.upperFirst(result);
+//   return result;
+// };
+
+// console.log(fixNamess(name));
